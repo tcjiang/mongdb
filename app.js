53e7723e2695580229edb465d43a7122d2193970
tj <oliver8709@gmail.com>
2017-03-26 17:39:23 -0400

    update app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+var express = require('express'),
+    app = express(),
+    engines = require('consolidate'),
+    bodyParser = require('body-parser'),
+    MongoClient = require('mongodb').MongoClient,
+    assert = require('assert');
